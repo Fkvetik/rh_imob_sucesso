9e94c89454cbc9ebb5dd54b43d1c9ae99e161596
Fkvetik <flucasvagas@gmail.com>
2026-05-19 19:43:38 -0300

    fix: inclui arquivos estaticos antigos no deploy Next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  outputFileTracingIncludes: {
+    '/*': [
+      './index.html',
+      './novos-talentos.html',
+      './corretores.html',
+      './vagas.html',
+      './politica.html',
+      './404.html',
+      './styles.css',
+      './script.js',
+      './novos-talentos.css',
+      './novos-talentos.js',
+      './corretores.css',
+      './corretores.js',
+      './favicon.ico',
+      './favicon.svg',
+      './site.webmanifest',
+      './assets/**/*'
+    ]
+  }
 };
 module.exports = nextConfig;
